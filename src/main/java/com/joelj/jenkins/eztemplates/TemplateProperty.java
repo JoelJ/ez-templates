23f38503cc5f5af5d36810389b90a877c749952a
Joel <johnson.joel.86@gmail.com>
2013-02-26 12:28:41 -0700

    Template can now track it's implementations. When you re-save the template, it'll grab it from the previous configuration. Eventually implementations will register themselves with their templates.

--- src/main/java/com/joelj/jenkins/eztemplates/TemplateProperty.java
+++ src/main/java/com/joelj/jenkins/eztemplates/TemplateProperty.java
@@ -5,8 +5,12 @@ import hudson.model.AbstractProject;
 import hudson.model.JobProperty;
 import hudson.model.JobPropertyDescriptor;
 import net.sf.json.JSONObject;
+import org.kohsuke.stapler.Ancestor;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * User: Joel Johnson
@@ -14,8 +18,15 @@ import org.kohsuke.stapler.StaplerRequest;
  * Time: 10:11 PM
  */
 public class TemplateProperty extends JobProperty<AbstractProject<?,?>> {
+	private final List<String> implementations;
+
 	@DataBoundConstructor
-	public TemplateProperty() {
+	public TemplateProperty(List<String> implementations) {
+		this.implementations = implementations;
+	}
+
+	public List<String> getImplementations() {
+		return implementations;
 	}
 
 	@Extension
@@ -23,9 +34,32 @@ public class TemplateProperty extends JobProperty<AbstractProject<?,?>> {
 		@Override
 		public JobProperty<?> newInstance(StaplerRequest req, JSONObject formData) throws FormException {
 			if(formData.size() > 0) {
-				return new TemplateProperty();
+				List<String> implementationJobs = new LinkedList<String>();
+				Ancestor ancestor = req.getAncestors().get(req.getAncestors().size() - 1);
+				while(ancestor != null && !(ancestor.getObject() instanceof AbstractProject)) {
+					ancestor = ancestor.getPrev();
+				}
+
+				if(ancestor != null) {
+					AbstractProject thisProject = (AbstractProject) ancestor.getObject();
+
+					@SuppressWarnings("unchecked")
+					TemplateProperty property = (TemplateProperty) thisProject.getProperty(TemplateProperty.class);
+
+					if(property != null) {
+						List<String> oldImplementationList = property.getImplementations();
+						implementationJobs.addAll(oldImplementationList);
+					}
+				}
+
+				return new TemplateProperty(implementationJobs);
 			}
 			return null;
+		}
+
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+			return super.configure(req, json);
 		}
 
 		@Override
