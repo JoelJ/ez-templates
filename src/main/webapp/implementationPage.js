72684ed0bec5dc753d77b33302972e4bdda82f1f
Joel <johnson.joel.86@gmail.com>
2013-02-26 19:07:53 -0700

    Show a warning instead of trying to hide everything.

--- src/main/webapp/implementationPage.js
+++ src/main/webapp/implementationPage.js
@@ -1,25 +1,10 @@
 TemplateImplementationPage = {
-	keepVisibleFields: [
-		'[name="useTemplate"]',
-		'[name="name"]',
-		'[name="parameterized"]',
-//		'.section-header',
-		'[name*="Trigger"]',
-		'[name*="trigger"]',
-		'[name="parameter"]',
-		'.apply-button',
-		'[name="description"]',
-		'[name="_.templateJobName"]'
-	],
-
 	hideConfiguration: function() {
-		console.log("hiding configuration");
-		$$('form[name="config"]').first().addClassName('disabled')
+		$('templateImplementationWarning').addClassName("show");
 	},
 
 	showConfiguration: function() {
-		console.log("showing configuration");
-		$$('form[name="config"]').first().removeClassName('disabled')
+		$('templateImplementationWarning').removeClassName("show");
 	},
 
 	checkboxChecked: function(event) {
@@ -39,15 +24,4 @@ Event.observe(window, 'load', function() {
 	}
 
 	implementTemplateCheckbox.observe('click', TemplateImplementationPage.checkboxChecked);
-
-	TemplateImplementationPage.keepVisibleFields.forEach(function(selector) {
-		var selected = $$(selector);
-		if(selected) {
-			selected.forEach(function(element) {
-				element.up('tr').addClassName('enabled');
-			});
-		}
-	});
-
-	$$('input[name="pseudoUpstreamTrigger"]').first().up('tr').previousSibling.addClassName('enabled');
 });
